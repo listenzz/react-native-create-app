6a02027e3dc6ec6309cecd378f8dacea3af9bc89
listen <listenzz@163.com>
2020-02-26 00:38:21 +0800

    update

--- templates/ios.js
+++ templates/ios.js
@@ -1,7 +1,9 @@
 module.exports = platform => [
   {
     name: () => "ios/Podfile",
-    content: ({ name }) => `source 'https://github.com/CocoaPods/Specs.git'
+    content: ({
+      name
+    }) => `source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
     
 platform :ios, '9.0'
 inhibit_all_warnings!
